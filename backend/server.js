c02853edb586f4c6bfc0a93de3988fa91e2399c8
mienpham04 <phammocmien04@gmail.com>
2024-03-12 13:33:56 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 
 const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT;
 
-app.listen(5000, () => console.log(`Server running on port 5000`));
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+app.listen(5000, 
+    () => console.log(`Server running on port ${PORT}`));
 
