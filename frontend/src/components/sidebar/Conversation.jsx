264c29a7af7a71da2b60d2ab81ca8cef9f2ac542
mienpham04 <phammocmien04@gmail.com>
2024-03-15 04:42:45 -0400

    Complete UI

---
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -0,0 +1,22 @@
+const Conversation = () => {
+  return (
+    <>
+      <div className="flex gap-2 items-center hover:bg-green-300 rounded p-2 py-1 cursor-pointer">
+        <div className="avatar online">
+          <img src="" alt="user avatar" />
+        </div>
+      </div>
+
+      <div className="flex flex-col flex-1">
+        <div className="flex gap-3 justify-between">
+          <p className="font-bold text-gray-200">John Doe</p>
+          <span className="text-xl">^^</span>
+        </div>
+      </div>
+
+      <div className="divider my-0 py-0 h-1" />
+    </>
+  );
+};
+
+export default Conversation;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
+import MessageContainer from "../../components/messages/MessageContainer"
 import Sidebar from "../../components/sidebar/Sidebar"
 
 const Home = () => {
   return (
     <div className="flex sm:h-[450px] md:h-[550px] bg-green-300 overflow-hidden rounded-lg bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10">
       <Sidebar />
+
+      <MessageContainer />
     </div>
   )
 }
