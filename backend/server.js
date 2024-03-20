bf1c1ea8f004011eba82cb3f0c843e3a4b2728ea
mienpham04 <phammocmien04@gmail.com>
2024-03-19 23:22:11 -0400

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import path from "path";
 
-import userRoutes from "./routes/user.routes.js"
-import authRoutes from "./routes/auth.routes.js"
-import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 
-import connectToMongoDB from './db/connectToMongoDB.js';
-import { app, server } from "./socket/socket.js"
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json()); // to parse requests from req.body
 app.use(cookieParser());
@@ -25,12 +28,17 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-})
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-server.listen(PORT, () => {
-    connectToMongoDB();
-    console.log(`Server running on port ${PORT}`)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
+// app.get("/", (req, res) => {
+//     res.send("Hello World");
+// })
+
+server.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server running on port ${PORT}`);
+});
